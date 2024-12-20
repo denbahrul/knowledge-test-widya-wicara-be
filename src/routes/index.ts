d0cdc639ad799f18f26ddba32b5eb48925f7d145
denbahrul <ulum0326@gmail.com>
2024-12-20 18:06:36 +0700

    feat: authentication middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { userRoute } from "./user.routers";
 import { productRoute } from "./product.routers";
+import { authentication } from "../middlewares/authentication";
 
 export const router = Router();
 
 router.use("/user", userRoute);
-router.use("/product", productRoute);
+router.use("/product", authentication, productRoute);
